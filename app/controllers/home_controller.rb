819ac4c6cac66d47134c40e3b11a66fe311bc443
Blake Yoder <yoderblake@gmail.com>
2017-02-26 18:33:33 -0500

    Added blank check for events wrapper

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,9 @@ class HomeController < ApplicationController
     before_filter :disable_cart_partial
     def home
     	untappd = Untappd.new
+        # hardcoded values passed in from client credentials
         @events = untappd.get_events(2652)
         @beers = untappd.get_list(17868)
     end
-    
+
 end
